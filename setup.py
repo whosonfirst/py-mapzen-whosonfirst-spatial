ccaf0cabf9aa8d8b51bcec66d84259f53288f1e4
thisisaaronland <aaron@mapzen.com>
2015-09-29 23:12:14 +0000

    update dependency links; bump to 0.03

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ desc = open("README.md").read(),
 setup(
     name='mapzen.whosonfirst.spatial',
     namespace_packages=['mapzen', 'mapzen.whosonfirst', 'mapzen.whosonfirst.spatial'],
-    version='0.02',
+    version='0.03',
     description='Simple Python wrapper for Who\'s On First spatial functionality',
     author='Mapzen',
     url='https://github.com/mapzen/py-mapzen-whosonfirst-spatial',
@@ -18,6 +18,7 @@ setup(
         'mapzen.whosonfirst.utils',
         ],
     dependency_links=[
+        'https://github.com/whosonfirst/py-mapzen-whosonfirst-utils/tarball/master#egg=mapzen.whosonfirst.utils-0.05',
         ],
     packages=packages,
     scripts=[
@@ -25,5 +26,5 @@ setup(
         'scripts/wof-spatial-query',
         'scripts/wof-spatial-server.py',
         ],
-    download_url='https://github.com/mapzen/py-mapzen-whosonfirst-spatial/releases/tag/v0.02',
+    download_url='https://github.com/mapzen/py-mapzen-whosonfirst-spatial/releases/tag/v0.03',
     license='BSD')
