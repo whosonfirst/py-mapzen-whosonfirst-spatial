b6b9a06a3f2022b7db523778d2161557cf19d6d2
stepps00 <stephenepps00@gmail.com>
2018-05-03 16:05:02 -0700

    updates for sqlite support

--- mapzen/whosonfirst/spatial/sqlite.py
+++ mapzen/whosonfirst/spatial/sqlite.py
@@ -5,23 +5,26 @@ import mapzen.whosonfirst.spatial
 import time
 import logging
 import sqlite3
+import json
 
 class spatialite(mapzen.whosonfirst.spatial.base):
 
     def __init__(self, **kwargs):
 
-        dsn = kwargs.get("dsn", ":memory")
+        dsn = kwargs.get("dsn", "/Users/stephen.epps/Desktop/sqlite-db/whosonfirst-data-latest.db")
         conn = sqlite3.connect(dsn)
 
         conn.enable_load_extension(True)
-        conn.execute('SELECT load_extension("libspatialite")')
-        conn.execute('SELECT InitSpatialMetaData();')
+
+        conn.execute('SELECT load_extension("mod_spatialite.dylib")')
+        # if we load a real DB we don't need to init it
+        #conn.execute('SELECT InitSpatialMetaData();')
 
         self.curs = conn.cursor()
 
     def point_in_polygon(self, lat, lon, **kwargs):
 
-	sql = "SELECT id FROM geometries WHERE ST_Within(GeomFromText('POINT(%0.6f %0.6f)'), geom) AND rowid IN (SELECT pkid FROM idx_geometries_geom WHERE xmin < %0.6f AND xmax > %0.6f AND ymin < %0.6f AND ymax > %0.6f)" % (lon, lat, lon, lon, lat, lat)
+        sql = "SELECT id FROM geometries WHERE ST_Within(GeomFromText('POINT(%0.6f %0.6f)'), geom) AND rowid IN (SELECT pkid FROM idx_geometries_geom WHERE xmin < %0.6f AND xmax > %0.6f AND ymin < %0.6f AND ymax > %0.6f)" % (lon, lat, lon, lon, lat, lat)
 
         params = []
 
@@ -40,8 +43,7 @@ class spatialite(mapzen.whosonfirst.spatial.base):
         logging.debug("[spatial][spatialit][pip] time to execute query (point-in-polygon for %s,%s) : %s" % (lat, lon, ttx))
 
         for row in self.curs.fetchall():
-
-            row = self.inflate_row(row, **kwargs)
+            row = self.inflate_row(row)
 
             if not row:
                 logging.debug("[spatial][spatialite][pip] failed to inflate row")
@@ -49,7 +51,7 @@ class spatialite(mapzen.whosonfirst.spatial.base):
 
             yield row
 
-    def inflate_row(row, **kwargs):
+    def inflate_row(self, row):
 
         wofid = row[0]
 
@@ -57,7 +59,7 @@ class spatialite(mapzen.whosonfirst.spatial.base):
         params = [ wofid ]
 
         self.curs.execute(sql, params)
-        row = self.curs.fetch()
+        row = self.curs.fetchone()
 
         feature = json.loads(row[0])
         return feature
@@ -69,7 +71,41 @@ class spatialite(mapzen.whosonfirst.spatial.base):
         raise Exception, "Method 'row_to_feature' not implemented by this class."
 
     def row_to_feature(self, row, **kwargs):
-        raise Exception, "Method 'row_to_feature' not implemented by this class."
+        wofid = row["properties"]["wof:id"]
+        parent_id = row["properties"]["wof:parent_id"]
+        centroid = [ row["properties"]["geom:longitude"], row["properties"]["geom:latitude"] ]
+
+        props = row["properties"]
+
+        if row["geometry"]:
+
+            geom = row["geometry"]
+
+        else:
+
+            logging.warning("[spatial][postgis][row_to_feature] failed to parse geom (%s) for %s, because %s" % (geom, wofid, e))
+
+        if not centroid:
+
+            logging.warning("[spatial][postgis][row_to_feature] failed to parse centroid (%s) for %s, because %s" % (centroid, wofid, e))
+
+        if not geom and not centroid:
+
+            logging.error("[spatial][postgis][row_to_feature] can't parse either geom or centroid xxxx for %s" % wofid)
+            raise Exception, "bunk geometry for %s" % wofid
+
+        if not geom:
+            geom = centroid
+
+        if not centroid:
+
+            shp = shapely.geometry.asShape(geom)
+            coords = shp.centroid
+
+            props["geom:longitude"] = coords.x
+            props["geom:latitude"] = coords.y
+
+        return { 'type': 'Feature', 'geometry': geom, 'properties': props }
 
     def index_feature(self, feature, **kwargs):
         raise Exception, "Method 'index_feature' not implemented by this class."
