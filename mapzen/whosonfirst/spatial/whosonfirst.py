70b1f5cdcd356dccb1512f9586913be07333cf01
thisisaaronland <straup@gmail.com>
2018-07-11 12:18:54 -0700

    bump to 1.0.9; allow insecure connections to WOF API endpoints; update all the defaults to reflect a post-mapzen world

--- mapzen/whosonfirst/spatial/whosonfirst.py
+++ mapzen/whosonfirst/spatial/whosonfirst.py
@@ -8,17 +8,19 @@ import json
 
 import requests
 
+# as in the wof-pip-server this is part of go-whosonfirst-pip-v2
+
 class pip (mapzen.whosonfirst.spatial.base):
 
     def __init__(self, **kwargs):
 
         mapzen.whosonfirst.spatial.base.__init__(self, **kwargs)
 
-        self.scheme = kwargs.get('scheme', 'https')
-        self.hostname = kwargs.get('hostname', 'pip.mapzen.com')
-        self.port = kwargs.get('port', None)
+        self.scheme = kwargs.get('scheme', 'http')
+        self.hostname = kwargs.get('hostname', 'localhost')
+        self.port = kwargs.get('port', 8080)
 
-        self.data_root = kwargs.get("data_root", "https://whosonfirst.mapzen.com")
+        self.data_root = kwargs.get("data_root", "https://data.whosonfirst.org")
 
     def point_in_polygon(self, lat, lon, **kwargs):
 
@@ -62,26 +64,18 @@ class pip (mapzen.whosonfirst.spatial.base):
 
     def row_to_feature(self, row):
 
-        # this is what we have to work with... today (20170502/thisisaaronland)
-        # {u'Name': u'Utah', u'Deprecated': False, u'Superseded': False, u'Placetype': u'region', u'Offset': -1, u'Id': 85688567}
-
-        wofid = row["Id"]
+        wofid = row["wof:id"]
+        repo = row["wof:repo"]
 
         root = self.data_root
 
-        # please fix me (as in work out the details)
-        # (20170502/thisisaaronland)
-
-        """
-        repo = row.get("wof:repo", None)
-
-        if repo:
-            root = os.path.join(root, repo)
-        """
+        # please sort out using repo when fetching local files
 
         root = os.path.join(root, "data")
 
         return mapzen.whosonfirst.utils.load(root, wofid)
+
+# as in an endpoint that implements the whosonfirst-api
 
 class api (mapzen.whosonfirst.spatial.base):
 
@@ -89,17 +83,18 @@ class api (mapzen.whosonfirst.spatial.base):
 
         mapzen.whosonfirst.spatial.base.__init__(self, **kwargs)
 
-        self.endpoint = kwargs.get('endpoint', 'https://places.mapzen.com/v1')
-        self.api_key = kwargs.get('api_key', None)
+        self.endpoint = kwargs.get('endpoint', 'https://api.whosonfirst.org/rest')
+        self.access_token = kwargs.get('access_token', None)
 
-        self.data_root = kwargs.get("data_root", "https://whosonfirst.mapzen.com")
+        self.data_root = kwargs.get("data_root", "https://data.whosonfirst.org")
+        self.insecure = kwargs.get("insecure", False)
 
     def point_in_polygon(self, lat, lon, **kwargs):
 
         filters = kwargs.get("filters", {})
 
         params = {
-            "api_key": self.api_key,
+            "access_token": self.access_token,
             "method": "whosonfirst.places.getByLatLon",
             "latitude": lat,
             "longitude": lon,
@@ -114,7 +109,10 @@ class api (mapzen.whosonfirst.spatial.base):
 
         try:
 
-            rsp = requests.get(self.endpoint, params=params)
+            if self.insecure:
+                rsp = requests.get(self.endpoint, params=params, verify=False)
+            else:
+                rsp = requests.get(self.endpoint, params=params)
 
             if rsp.status_code != requests.codes.ok:
                 rsp.raise_for_status()
@@ -148,4 +146,4 @@ class api (mapzen.whosonfirst.spatial.base):
 
         root = os.path.join(root, "data")
 
-        return mapzen.whosonfirst.utils.load(root, wofid)
+        return mapzen.whosonfirst.utils.load(root, wofid, insecure=self.insecure)
